74cdd940f1f9cd68dc5afc41657d3a1ae55bd784
eliot-hash <eliot@beton.win>
2025-09-30 04:47:02 +0300

    chore: init monorepo (api-gateway, auth, event-handler, prisma, protos, shared)

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from './auth/dto/sing-in.dto';
+export * from './auth/dto/sing-up.dto';
+export * from './auth/dto/refresh.dto';
+
+export * from './event-handler/dto/view-content.dto';
+export * from './event-handler/dto/prepare-install-link.dto';
+export * from './event-handler/dto/first-open.dto';
+export * from './event-handler/dto/lead.dto';
+export * from './event-handler/dto/complete-registration.dto';
+export * from './event-handler/dto/purchase.dto';
+export * from './event-handler/dto/subscribe.dto';
+
